13c00c8b617f57d27e20db516183c787fcc4f676
ionescuig <ionescuig@yahoo.com>
2019-01-28 21:45:54 +0000

    Finalized main project and html_message template.

--- src/mtaxi/settings/production.py
+++ src/mtaxi/settings/production.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
